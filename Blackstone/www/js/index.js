a20f8577c4058522466d355d3f9bda2fc76f56b5
Matthew Zenn <spyguy57@gmail.com>
2024-06-28 19:24:40 -0400

    added tts

--- Blackstone/www/js/index.js
+++ Blackstone/www/js/index.js
@@ -1,41 +1,28 @@
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
-const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
-
-const grammar = "#JSGF V1.0; grammar commands; public <commands> = hello|generate an image of the card i'm thinking of;";
-
-const recognition = new SpeechRecognition();
-const speechRecognitionList = new SpeechGrammarList();
-speechRecognitionList.addFromString(grammar, 1);
-recognition.grammars = speechRecognitionList;
-recognition.continuous = false;
-
 var coin = 0;
 
 document.getElementById('btn').addEventListener('click', () => {
   if (coin == 0) {
     document.getElementById('btn').style.boxShadow = 'inset 5px 5px 10px #161b1d41';
     document.getElementById('btn').style.fontSize = '23pt';
-    recognition.start();
     coin = 1;
+    record();
   }
   else {
-    recognition.stop();
     document.getElementById('btn').style.boxShadow = 'none';
     document.getElementById('btn').style.fontSize = '24pt';
     coin = 0;
+    reply();
   }
 });
 
-recognition.onspeechend = () => {
-  recognition.stop();
-  document.getElementById('btn').style.boxShadow = 'none';
-  document.getElementById('btn').style.fontSize = '24pt';
-  coin = 0;
+function record() {
+  document.getElementById('btn').click();
 };
 
-// recognition.onresult = (event) => {
-//   const result = event.results[0][0].transcript;
-//   diagnostic.textContent = `${result}.`;
-//   // if then...
-// };
+function reply() {
+  TTS.speak({
+    text: "Ok, here are some images of the card you're thinking of.",
+    rate: 1.5,
+    cancel: true
+  });
+}
